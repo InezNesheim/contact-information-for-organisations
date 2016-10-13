db360b645874c42e462de302c4c2175d16369856
CORP\vingebre <vidar.ingebretsen@capgemini.com>
2016-10-13 09:25:00 +0200

    Add logging and comments

--- AltinnDesktopTool/RestClient/Controllers/IRestQueryController.cs
+++ AltinnDesktopTool/RestClient/Controllers/IRestQueryController.cs
@@ -1,7 +1,14 @@
 ﻿namespace RestClient.Controllers
 {
+
+    /// <summary>
+    /// The controller interface. Inherits the IRestQuery by adding the ControllerContext.
+    /// </summary>
     public interface IRestQueryController : IRestQuery
     {
+        /// <summary>
+        /// Contains the required data for the Controller.
+        /// </summary>
         ControllerContext Context { get; set; }
     }
 }
