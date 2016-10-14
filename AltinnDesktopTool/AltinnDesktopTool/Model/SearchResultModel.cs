535b68ac17774d7725b82b4f519c1202d03a05b0
CORP\vingebre <vidar.ingebretsen@capgemini.com>
2016-10-14 10:04:38 +0200

    Added Result View

---
+++ AltinnDesktopTool/AltinnDesktopTool/Model/SearchResultModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AltinnDesktopTool.Model
+{
+    public class SearchResultModel : ModelBase
+    {
+        private ObservableCollection<OrganizationModel> _resultCollection;
+
+        public ObservableCollection<OrganizationModel> ResultCollection
+        {
+          get { return _resultCollection; }
+          set
+            {
+                _resultCollection = value;
+                RaisePropertyChanged(() => ResultCollection);
+            }
+        }
+
+        public SearchResultModel()
+        {
+            ResultCollection = new ObservableCollection<OrganizationModel>();
+        }
+    }
+}
--- AltinnDesktopTool/AltinnDesktopTool/ViewModel/SearchResultViewModel.cs
+++ AltinnDesktopTool/AltinnDesktopTool/ViewModel/SearchResultViewModel.cs
@@ -5,6 +5,7 @@ using RestClient.DTO;
 
 using GalaSoft.MvvmLight;
 using log4net;
+using AltinnDesktopTool.Model;
 
 namespace AltinnDesktopTool.ViewModel
 {
@@ -15,13 +16,22 @@ namespace AltinnDesktopTool.ViewModel
         public SearchResultViewModel(ILog logger)
         {
             _logger = logger;
+            Model = new SearchResultModel();
 
             PubSub<IList<Organization>>.RegisterEvent(EventNames.SearchResultRecievedEvent, SearchResultRecievedEventHandler);
         }
 
+        public SearchResultModel Model { get; set; }
+
         public void SearchResultRecievedEventHandler(object sender, PubSubEventArgs<IList<Organization>> args)
         {
             _logger.Debug("Handling search result received event.");
+
+            Model.ResultCollection = new System.Collections.ObjectModel.ObservableCollection<OrganizationModel>()
+            {
+                new OrganizationModel() { Name = "My organization", OrganizationNumber = "orgno" },
+                new OrganizationModel() { Name = "Coca company", OrganizationNumber = "cola orgno" },
+            };
 
             // TODO Add result list to model bound to view
         }
