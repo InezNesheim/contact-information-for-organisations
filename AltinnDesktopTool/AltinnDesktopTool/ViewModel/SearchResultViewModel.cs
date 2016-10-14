7e5b9d649f9e92c1ba89267b95b2e30d2263bb9f
anetteha <anette.hagberg@gmail.com>
2016-10-14 10:07:06 +0200

    Added Extended WPF Toolkit & automapper

--- AltinnDesktopTool/AltinnDesktopTool/ViewModel/SearchResultViewModel.cs
+++ AltinnDesktopTool/AltinnDesktopTool/ViewModel/SearchResultViewModel.cs
@@ -11,7 +11,7 @@ namespace AltinnDesktopTool.ViewModel
     public class SearchResultViewModel : ViewModelBase
     {
         private readonly ILog _logger;
-
+        
         public SearchResultViewModel(ILog logger)
         {
             _logger = logger;
