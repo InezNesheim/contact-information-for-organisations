469579723f0cb42f7e516c9b7cbdc95333d5bc68
anetteha <anette.hagberg@gmail.com>
2016-10-17 13:28:13 +0200

    Added GetContactsCommand

--- AltinnDesktopTool/AltinnDesktopTool/ViewModel/SearchResultViewModel.cs
+++ AltinnDesktopTool/AltinnDesktopTool/ViewModel/SearchResultViewModel.cs
@@ -7,30 +7,46 @@ using GalaSoft.MvvmLight;
 using log4net;
 using AltinnDesktopTool.Model;
 using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight.Command;
 
 namespace AltinnDesktopTool.ViewModel
 {
     public class SearchResultViewModel : ViewModelBase
     {
         private readonly ILog _logger;
-        
+
         public SearchResultViewModel(ILog logger)
         {
             _logger = logger;
             Model = new SearchResultModel();
 
             PubSub<ObservableCollection<OrganizationModel>>.RegisterEvent(EventNames.SearchResultRecievedEvent, SearchResultRecievedEventHandler);
+
+            GetContactsCommand = new RelayCommand<OrganizationModel>(GetContactsCommandHandler);
+        }
+
+        private void GetContactsCommandHandler(OrganizationModel obj)
+        {
+            // TODO get contacts 
+
+            /*
+             <i:Interaction.Triggers>
+                <i:EventTrigger EventName="Click" >
+                    <i:InvokeCommandAction Command="{Binding GetContactsCommand}" CommandParameter="{Binding Item}"/>
+                </i:EventTrigger>
+            </i:Interaction.Triggers>
+             */
         }
 
         public SearchResultModel Model { get; set; }
+
+        public RelayCommand<OrganizationModel> GetContactsCommand { get; set; }
 
         public void SearchResultRecievedEventHandler(object sender, PubSubEventArgs<ObservableCollection<OrganizationModel>> args)
         {
             _logger.Debug("Handling search result received event.");
 
             Model.ResultCollection = args.Item;
-
-            // TODO Add result list to model bound to view
         }
     }
 }
