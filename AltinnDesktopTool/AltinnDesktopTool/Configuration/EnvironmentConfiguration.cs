ffa07d7641dc5dcdfdf4021a064e2c518da67d9c
Terje Holene <terje.holene@avanade.com>
2016-10-24 16:10:38 +0200

    Changes related to StyleCop and Code Analysis.

--- AltinnDesktopTool/AltinnDesktopTool/Configuration/EnvironmentConfiguration.cs
+++ AltinnDesktopTool/AltinnDesktopTool/Configuration/EnvironmentConfiguration.cs
@@ -4,6 +4,9 @@ namespace AltinnDesktopTool.Configuration
 {
     public class EnvironmentConfiguration : IUiEnvironmentConfig, IRestQueryConfig    
     {
+        /// <summary>
+        /// Gets or sets the name of the environment that this configuration is connected to.
+        /// </summary>
         public string Name { get; set; }
 
         public string ThemeName { get; set; }
